13997f2b373c97d7c651a7e8cd8ede3dd4de3a00
Justin Baur <admin@justinbaur.com>
2021-12-05 16:18:47 +0200

    Work on day1

---
+++ day1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let filename = "./input.txt";
+    let numbers = read_lines(filename);
+    println!("{}", part1(numbers));
+    
+}
+
+fn part1(numbers: Vec<i32>) -> i32 {
+    let mut increase_count = 0;
+    let mut prev_value = numbers[0];
+    for i in 1..numbers.len() {
+        let current_value = numbers[i];
+        if current_value > prev_value {
+            increase_count += 1;
+        }
+        prev_value = current_value;
+    }
+    increase_count
+}
+
+fn read_lines(filename: impl AsRef<Path>) -> Vec<i32> {
+    let file = File::open(filename)
+        .expect("Failed to open file");
+
+    io::BufReader::new(file)
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .map(|l| l.parse::<i32>().expect("Could not parse line"))
+        .collect()
+}
